2fb7abd4be0794ddb6426dab3238d2b32ccb34c4
sorenpeter <sp@darch.dk>
2023-09-22 11:49:14 +0200

    2nd2nd

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once("partials/base.php");
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Timeline</title>
 </head>
 <body >
